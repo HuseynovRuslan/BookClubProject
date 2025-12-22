0b04cb99dbf8ed4ffdbd7e6c1d01943574eeab24
HuseynovRuslan <155317262+HuseynovRuslan@users.noreply.github.com>
2025-12-23 01:20:03 +0400

    fix-cors-error

--- Backend/Goodreads.API/Extensions/DependencyInjection.cs
+++ Backend/Goodreads.API/Extensions/DependencyInjection.cs
@@ -18,7 +18,21 @@ public static class DependencyInjection
         {
             options.AddPolicy("AllowFrontend", policy =>
             {
-                policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "http://localhost:5174")
+                policy.WithOrigins(
+                        // Frontend origins
+                        "http://localhost:5173", 
+                        "https://localhost:5173",
+                        "http://localhost:3000", 
+                        "https://localhost:3000",
+                        "http://localhost:5174",
+                        "https://localhost:5174",
+                        "http://localhost:5175",
+                        "https://localhost:5175",
+                        
+                        "http://localhost:5062",
+                        "https://localhost:7050",
+                        "http://localhost:15357",
+                        "https://localhost:44324")
                       .AllowAnyMethod()
                       .AllowAnyHeader()
                       .AllowCredentials();
